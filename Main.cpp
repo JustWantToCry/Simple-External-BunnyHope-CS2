493eafa2d069e192416c583b227978a9ed8a24e1
JustWantToCry <161080574+JustWantToCry@users.noreply.github.com>
2024-02-23 23:38:38 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,19 @@
+//Code created by JustWantToCry. You can find me in Discord
+//For use this code just replace my memory Read/Write method on method from your source
+
 #include <thread>
 #include <iostream>
 
 int main()
 {
 
-    auto localPlayer = VARS::memRead<uintptr_t>(VARS::baseAddress + 0x1729348);//
+    auto localPlayer = VARS::memRead<uintptr_t>(VARS::baseAddress + 0x1729348); //dwLocalPlayerPawn offset
+    auto dwForceJump = VARS::memRead<uintptr_t>(VARS::baseAddress + 0x17226E0); //dwForceJump offset
+    auto m_fFlags = VARS::memRead<uintptr_t>(localPlayer + 0x3D4);//C_BaseEntity m_fFlags offset
 
     while (true)
     {
 #pragma region BunnyHope
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
 
         if (!localPlayer) {
             continue;
@@ -18,9 +21,9 @@ int main()
 
         if (GetAsyncKeyState(VK_SPACE) && m_fFlags & (1 << 0)) {
 
-            VARS::memWrite(VARS::baseAddress + 0x17226E0, 65537);
+            VARS::memWrite(dwForceJump, 65537);
             std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            VARS::memWrite(VARS::baseAddress + 0x17226E0, 256);
+            VARS::memWrite(dwForceJump, 256);
 
 
         }
